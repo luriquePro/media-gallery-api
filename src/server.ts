b6d85b20266f43dc8018c4eb49b9a63dd64456f0
LuizProgHenri <luiz.henrique@arenafantasy.com>
2025-02-13 07:49:55 -0300

    Ref: Refatorando o Server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import moment from "moment";
 import { ApplicationAPI } from "./app.js";
 
-new ApplicationAPI().listen(Number(process.env.PORT), () =>
-	console.log(`Servidor iniciado. \nRodando na porta: ${process.env.PORT}. \nData: ${moment().format("DD/MM/YY HH:MM:SS")}.`),
-);
+const PORT = Number(process.env.PORT);
+const app = new ApplicationAPI();
+
+const cb = () => {
+	console.log(`Servidor iniciado. \nRodando na porta: ${process.env.PORT}. \nData: ${moment().format("DD/MM/YY HH:MM:SS")}.`);
+};
+
+app.listen(PORT, cb);
